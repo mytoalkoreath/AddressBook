9d0f4bb2b705539337ff2383af82831d5dc14999
matthewfundarek <mcfundarek@hotmail.com>
2020-02-07 15:28:36 -0500

    Tests

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,29 +1,31 @@
 
 
 
+import SpringApp.AddressBook;
+
 import static org.junit.Assert.*;
 
 
 public class AddressBookTest {
-    AddressBook test = new AddressBook("Book 1");
+    AddressBook test = new AddressBook();
 
     @org.junit.Test
     public void addBuddy() {
         assertEquals("AddressBook{buddies=[]}", test.toString());
-        test.addBuddy("Test", "1234");
-        assertEquals("AddressBook{buddies=[BuddyInfo{pNumber='1234', name='Test'}]}", test.toString());
+        test.addBuddy("Test", "1234", "432 test lane");
+        assertEquals("AddressBook{buddies=[BuddyInfo{name='Test', pNumber='1234', address='432 test lane'}]}", test.toString());
     }
 
     @org.junit.Test
     public void testToString() {
 
-        test.addBuddy("Test2", "12345");
-        assertEquals("AddressBook{buddies=[BuddyInfo{pNumber='12345', name='Test2'}]}", test.toString());
+        test.addBuddy("Test2", "12345", "432 test lane");
+        assertEquals("AddressBook{buddies=[BuddyInfo{name='Test2', pNumber='12345', address='432 test lane'}]}", test.toString());
     }
 
     @org.junit.Test
     public void testRemoveBuddy() {
-        test.addBuddy("Test", "1234");
+        test.addBuddy("Test", "1234", "432 test lane");
         assertTrue(test.removeBuddy("Test"));
 
 
--- src/test/java/BuddyInfoTest.java
+++ src/test/java/BuddyInfoTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.*;
 
 
 public class BuddyInfoTest {
-    private BuddyInfo test = new BuddyInfo("Test", "1234");
+    private BuddyInfo test = new BuddyInfo("Test", "1234", "432 test lane");
 
 
     @org.junit.Test
     public void testToString() {
-        assertTrue(test.toString().equals("BuddyInfo{pNumber='1234', name='Test'}"));
+        assertTrue(test.toString().equals("BuddyInfo{name='Test', pNumber='1234', address='432 test lane'}"));
 
     }
 
@@ -37,5 +37,16 @@ public class BuddyInfoTest {
     public void testSetName() {
         test.setName("tseT");
         assertTrue(test.getName().equals("tseT"));
+    }
+
+    @org.junit.Test
+    public void testGetAddress() {
+        assertTrue(test.getAddress().equals("432 test lane"));
+    }
+
+    @org.junit.Test
+    public void testSetAddress() {
+        test.setName("Not 432 test lane");
+        assertTrue(test.getAddress().equals("Not 432 test lane"));
     }
 }
