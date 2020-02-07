0197b3517839108cdf23be06f76eb96d83625cb5
matthewfundarek <mcfundarek@hotmail.com>
2020-02-07 16:55:21 -0500

    Tests

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -1,8 +1,10 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import SpringApp.Greeting;
 import SpringApp.HandlingFormSubmissionApplication;
 import org.junit.jupiter.api.Test;
 import SpringApp.GreetingController;
+import org.springframework.ui.Model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +14,13 @@ public class SmokeTest {
 
     @Autowired
     private GreetingController controller;
+    private Model m;
+
 
     @Test
     public void contexLoads() throws Exception {
         assertThat(controller).isNotNull();
     }
+
+
 }
--- src/test/java/TestingWebApplicationTest.java
+++ src/test/java/TestingWebApplicationTest.java
@@ -1,15 +1,24 @@
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import SpringApp.Greeting;
 import SpringApp.HandlingFormSubmissionApplication;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 
 @SpringBootTest(classes={HandlingFormSubmissionApplication.class})
@@ -21,6 +30,45 @@ public class TestingWebApplicationTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/create")).andDo(print()).andExpect(status().isOk());
+        Greeting g = new Greeting();
+        g.setId("Book1");
+        this.mockMvc.perform(post("/")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("Id", "Book1"))
+                .andDo(print()).andExpect(status().isOk()).andExpect(content()
+                .string(containsString("AddressBook Created!")));
+        this.mockMvc.perform(post("/create")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("Id", "Frank")
+                .param("content", "123")
+                .param("address", "1234 test ave"))
+                .andDo(print()).andExpect(status().isOk()).andExpect(content()
+                .string(containsString("Frank")));
+        this.mockMvc.perform(post("/create")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("Id", "George")
+                .param("content", "123")
+                .param("address", "1234 test ave"))
+                .andDo(print()).andExpect(status().isOk()).andExpect(content()
+                .string(containsString("George")));
+        this.mockMvc.perform(post("/delete")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("Id", "Frank"))
+                .andDo(print()).andExpect(status().isOk()).andExpect(content()
+                .string(containsString("George")));
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
 }
